24538d72e36a2e0226ad64f0ecc53420c6f5cf17
Chris <zhris@zhr.is>
2025-01-01 11:55:40 -0600

    Initial Commit

---
+++ System Settings/System_SettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  System_SettingsApp.swift
+//  System Settings
+//
+//  Created by Chris on 1/1/25.
+//
+
+import SwiftUI
+
+@main
+struct System_SettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
