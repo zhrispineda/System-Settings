32e781b82764696f80aea0fabbd1bfa7872c432b
Chris <zhrispineda@icloud.com>
2025-05-04 13:19:34 -0400

    Add appearance assets and localization

--- System Settings/Views/AppearanceView.swift
+++ System Settings/Views/AppearanceView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct AppearanceView: View {
     @State private var selectedTheme = "AUTO"
+    @State private var selectedAccent = "Multicolor"
+    @State private var selectedAccentColor = Color.blue
     @State private var selectedHighlight = "Accent Color"
     @State private var selectedSidebar = "SIDEBAR_ICON_MEDIUM"
     @State private var selectedScroll = 1
@@ -19,51 +21,79 @@ struct AppearanceView: View {
     let sidebarOptions = ["SIDEBAR_ICON_SMALL", "SIDEBAR_ICON_MEDIUM", "SIDEBAR_ICON_LARGE"]
     let scrollOptions = ["SHOW_SCROLL_AUTOMATICALLY", "SHOW_SCROLL_WHEN_SCROLLING", "SHOW_SCROLL_ALWAYS"]
     let clickOptions = ["NEXT_PAGE", "GO_THERE"]
+    let table = "Appearance"
     
     var body: some View {
-        CustomForm(title: "APPEARANCE") {
+        CustomForm(title: "APPEARANCE".localize(table: table)) {
             Section {
                 HStack(alignment: .top) {
-                    Text("APPEARANCE")
+                    Text("APPEARANCE", tableName: table)
                     Spacer()
                     
-                    DisplayButton(option: "LIGHT", helpText: "LIGHT_TOOLTIP", image: .appearanceLight, selected: $selectedTheme)
+                    DisplayButton(option: "LIGHT", helpText: "LIGHT_TOOLTIP", image: .appearanceLight, selected: $selectedTheme, color: $selectedAccentColor)
                     
-                    DisplayButton(option: "DARK", helpText: "Uses a dark appearance for buttons, menus, and windows.", image: .appearanceDark, selected: $selectedTheme)
+                    DisplayButton(option: "DARK", helpText: "DARK_TOOLTIP", image: .appearanceDark, selected: $selectedTheme, color: $selectedAccentColor)
                     
-                    DisplayButton(option: "AUTO", helpText: "AUTO_TOOLTIP", image: .appearanceAuto, selected: $selectedTheme)
+                    DisplayButton(option: "AUTO", helpText: "AUTO_TOOLTIP", image: .appearanceAuto, selected: $selectedTheme, color: $selectedAccentColor)
                 }
                 
-                Text("ACCENT_COLOR_TITLE")
+                HStack(alignment: .top) {
+                    Text("ACCENT_COLOR_TITLE", tableName: table)
+                    Spacer()
+                    AccentButton(label: "Multicolor", option: .clear, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Blue", option: .blue, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Purple", option: .purple, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Pink", option: .pink, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Red", option: .red, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Orange", option: .orange, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Yellow", option: .yellow, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Green", option: .green, selected: $selectedAccent, accent: $selectedAccentColor)
+                    AccentButton(label: "Graphite", option: .gray, selected: $selectedAccent, accent: $selectedAccentColor)
+                }
+                .onChange(of: selectedAccent) {
+                    if selectedAccent == "Multicolor" {
+                        selectedHighlight = "Accent Color"
+                    } else {
+                        selectedHighlight = selectedAccent
+                    }
+                }
                 
-                Picker("HIGHLIGHT_COLOR_TITLE", selection: $selectedHighlight) {
+                Picker("HIGHLIGHT_COLOR_TITLE".localize(table: table), selection: $selectedHighlight) {
                     ForEach(highlightOptions, id: \.self) { color in
-                        Text(color)
-                        
+                        HStack {
+                            switch color {
+                            case "Accent Color":
+                                Image(.nsMulticolorHighlight)
+                            default:
+                                EmptyView()
+                            }
+                            Text(color)
+                        }
+                        .selectionDisabled(selectedAccent != "Multicolor" && color == "Accent Color")
                     }
                 }
                 
-                Picker("SIDEBAR_ICON_SIZE_TITLE", selection: $selectedSidebar) {
+                Picker("SIDEBAR_ICON_SIZE_TITLE".localize(table: table), selection: $selectedSidebar) {
                     ForEach(sidebarOptions, id: \.self) { size in
-                        Text(size)
+                        Text(size.localize(table: table))
                     }
                 }
                 
-                Toggle("ALLOW_WALLPAPER_TINTING_TITLE", isOn: $wallpaperTinting)
+                Toggle("ALLOW_WALLPAPER_TINTING_TITLE".localize(table: table), isOn: $wallpaperTinting)
             }
             
             Section {
-                Picker(selection: $selectedScroll, label: Text("SHOW_SCROLL_TITLE")) {
-                    Text("SHOW_SCROLL_AUTOMATICALLY").tag(1)
-                    Text("SHOW_SCROLL_WHEN_SCROLLING").tag(2)
-                    Text("SHOW_SCROLL_ALWAYS").tag(3)
+                Picker(selection: $selectedScroll, label: Text("SHOW_SCROLL_TITLE", tableName: table)) {
+                    Text("SHOW_SCROLL_AUTOMATICALLY", tableName: table).tag(1)
+                    Text("SHOW_SCROLL_WHEN_SCROLLING", tableName: table).tag(2)
+                    Text("SHOW_SCROLL_ALWAYS", tableName: table).tag(3)
                 }
                 .pickerStyle(.radioGroup)
                 
                 VStack {
-                    Picker("SCROLLER_CLICKS_TITLE", selection: $selectedClick) {
+                    Picker("SCROLLER_CLICKS_TITLE".localize(table: table), selection: $selectedClick) {
                         ForEach(clickOptions, id: \.self) { option in
-                            Text(option)
+                            Text(option.localize(table: table))
                         }
                     }
                     .pickerStyle(.radioGroup)
@@ -75,30 +105,86 @@ struct AppearanceView: View {
     }
 }
 
+struct AccentButton: View {
+    let label: String
+    let option: Color
+    @Binding var selected: String
+    @Binding var accent: Color
+    
+    var body: some View {
+        VStack {
+            Button {
+                accent = option == .clear ? .blue : option
+                selected = label
+            } label: {
+                VStack {
+                    ZStack {
+                        if option == .clear {
+                            Image(.nsMulticolorAccent)
+                                .mask(Circle())
+                        } else {
+                            Circle()
+                                .foregroundStyle(option)
+                        }
+                        Circle()
+                            .stroke(.white.opacity(0.2), lineWidth: 1)
+                            .frame(width: 15, height: 15)
+                        if label == selected {
+                            Circle()
+                                .foregroundStyle(.white)
+                                .frame(width: 5, height: 5)
+                                .shadow(radius: 1)
+                        }
+                    }
+                    .frame(width: 16, height: 16)
+                    .foregroundStyle(.blue)
+                }
+            }
+            .buttonStyle(.plain)
+            .help(label)
+            
+            if label == selected {
+                Text(label)
+                    .foregroundStyle(.secondary)
+                    .fixedSize()
+                    .frame(width: 15)
+                    .lineLimit(1)
+                    .font(.footnote)
+                    .offset(x: option == .gray ? -13 : 0)
+            }
+        }
+    }
+}
+
 struct DisplayButton: View {
     let option: String
     let helpText: String
     let image: ImageResource
     @Binding var selected: String
+    @Binding var color: Color
     
     var body: some View {
         Button {
             selected = option
         } label: {
             VStack {
-                Image(image)
-                    .background {
-                        RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(selected == option ? .blue : .clear, lineWidth: 6)
-                        RoundedRectangle(cornerRadius: 5.0)
-                            .stroke(selected == option ? .white : .clear, lineWidth: 1)
-                    }
-                Text(option)
+                ZStack {
+                    Image(image)
+                        .background {
+                            RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(selected == option ? color : .clear, lineWidth: 6)
+                            RoundedRectangle(cornerRadius: 5.0)
+                                .stroke(selected == option ? .white : .clear, lineWidth: 1)
+                        }
+                    Image(option == "AUTO" ? .selectionColorMaskAuto : .selectionColorMask)
+                        .foregroundStyle(color)
+                }
+                Text(option.localize(table: "Appearance"))
                     .font(.callout)
                     .fontWeight(selected == option ? .semibold : .regular)
                     .foregroundStyle(selected == option ? .primary : .secondary)
             }
-            .help(helpText)
+            .help(helpText.localize(table: "Appearance"))
         }
         .buttonStyle(.plain)
     }
