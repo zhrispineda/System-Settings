5515ae1a6e89d3f9d13e5366d99b009007063c02
Chris <zhrispineda@icloud.com>
2025-05-13 15:36:02 -0400

    Update ContentView for searchText

--- System Settings/ContentView.swift
+++ System Settings/ContentView.swift
@@ -21,48 +21,71 @@ struct ContentView: View {
                 .scrollDisabled(true)
                 .searchable(text: $searchText, placement: .sidebar)
                 .navigationSplitViewColumnWidth(215)
-            List(selection: $selection) {
-                // MARK: Apple Account
-                Section {
-                    ForEach(accountOptions) { setting in
-                        NavigationLink(value: setting) {
-                            AppleAccountCell()
-                                .overlay {
-                                    DividerGeometryView(dividerOpacity: $dividerOpacity)
-                                }
+            if searchText.isEmpty {
+                List(selection: $selection) {
+                    // MARK: Apple Account
+                    Section {
+                        ForEach(accountOptions) { setting in
+                            NavigationLink(value: setting) {
+                                AppleAccountCell()
+                                    .overlay {
+                                        DividerGeometryView(dividerOpacity: $dividerOpacity)
+                                    }
+                            }
                         }
                     }
+                    
+                    // MARK: Radio + Battery/Power
+                    SettingsSection(path: $path, selection: $selection, options: radioOptions)
+                    
+                    // MARK: Main
+                    SettingsSection(path: $path, selection: $selection, options: mainOptions)
+                    
+                    // MARK: Focus
+                    SettingsSection(path: $path, selection: $selection, options: focusOptions)
+                    
+                    // MARK: Authentication
+                    SettingsSection(path: $path, selection: $selection, options: authOptions)
+                    
+                    // MARK: Services
+                    SettingsSection(path: $path, selection: $selection, options: serviceOptions)
+                    
+                    // MARK: Input
+                    SettingsSection(path: $path, selection: $selection, options: inputOptions)
                 }
-                
-                // MARK: Radio + Battery/Power
-                SettingsSection(path: $path, selection: $selection, options: radioOptions)
-                
-                // MARK: Main
-                SettingsSection(path: $path, selection: $selection, options: mainOptions)
-                
-                // MARK: Focus
-                SettingsSection(path: $path, selection: $selection, options: focusOptions)
-                
-                // MARK: Authentication
-                SettingsSection(path: $path, selection: $selection, options: authOptions)
-                
-                // MARK: Services
-                SettingsSection(path: $path, selection: $selection, options: serviceOptions)
-                
-                // MARK: Input
-                SettingsSection(path: $path, selection: $selection, options: inputOptions)
+                .focused($isFocused)
+                .opacity(appearsActive ? 1.0 : 0.5)
+                .onAppear {
+                    isFocused = true
+                }
+                .overlay {
+                    Divider()
+                        .frame(maxHeight: .infinity, alignment: .top)
+                        .opacity(dividerOpacity)
+                }
+                .toolbar(removing: .sidebarToggle)
+            } else {
+                GeometryReader { geo in
+                    List {
+                        VStack(alignment: .center) {
+                            Image(systemName: "magnifyingglass")
+                                .fontWeight(.thin)
+                                .padding(.bottom, 5)
+                            Text("No Results")
+                            Text("No results for “\(searchText)“")
+                                .foregroundStyle(.secondary)
+                                .font(.footnote)
+                        }
+                        .frame(maxWidth: .infinity, minHeight: 0, idealHeight: geo.size.height, maxHeight: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                        .listRowSeparator(.hidden)
+                    }
+                    .scrollDisabled(!searchText.isEmpty)
+                    .listStyle(.inset)
+                    .scrollContentBackground(.hidden)
+                    .toolbar(removing: .sidebarToggle)
+                }
             }
-            .focused($isFocused)
-            .opacity(appearsActive ? 1.0 : 0.5)
-            .onAppear {
-                isFocused = true
-            }
-            .overlay {
-                Divider()
-                    .frame(maxHeight: .infinity, alignment: .top)
-                    .opacity(dividerOpacity)
-            }
-            .toolbar(removing: .sidebarToggle)
         } detail: {
             NavigationStack(path: $path) {
                 selection?.destination
