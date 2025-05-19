9c8b2958f4a1b1c705fb7ed10da9ac3cdcb3d109
Chris <zhrispineda@icloud.com>
2025-05-19 11:09:58 -0400

    Update SiriView for localization

--- System Settings/Views/SiriView.swift
+++ System Settings/Views/SiriView.swift
@@ -17,19 +17,21 @@ struct SiriView: View {
     let listenOptions = ["“Siri“ or “Hey Siri“", "“Hey Siri“", "OFF_KEY"]
     let shortcutOptions = ["SAE_GLOBE_S", "LEFT_COMMAND_TWICE", "RIGHT_COMMAND_TWICE", "EITHER_COMMAND_TWICE", "CUSTOMIZE_KEY"]
     let languageOptions = ["English (United States)"]
+    let table = "SiriPreferenceExtension"
+    let exTable = "AssistantSettings-ChatGPT"
     
     var body: some View {
-        CustomForm(title: "SIRI_TITLE") {
+        CustomForm(title: "SIRI_TITLE".localize(table: table)) {
             Section {
                 HStack(alignment: .top, spacing: 15) {
-                    IconView("Apple Intelligence & Siri", color: .white)
+                    IconView("Apple Intelligence & Siri", color: .clear)
                         .symbolRenderingMode(.multicolor)
                         .scaleEffect(1.3)
                         .overlay {
                             ZStack {
                                 RoundedRectangle(cornerRadius: 15.0)
                                     .foregroundStyle(Color(NSColor.headerTextColor))
-                                Text("GM_BETA_LABEL")
+                                Text("GM_BETA_LABEL", tableName: table)
                                     .font(.system(size: 7))
                                     .foregroundStyle(Color(NSColor.gridColor))
                             }
@@ -37,7 +39,7 @@ struct SiriView: View {
                             .offset(x: 8, y: 10)
                         }
                     VStack(alignment: .leading) {
-                        Text("SIRI_SIDEBAR_TITLE_SAE")
+                        Text("SIRI_SIDEBAR_TITLE_SAE", tableName: table)
                         Text("\("A personal intelligence system integrated deeply into your Mac, apps, and Siri.") [\("Learn More…")](help:anchor=mchl46361784)")
                             .font(.subheadline)
                             .foregroundStyle(.secondary)
@@ -47,66 +49,66 @@ struct SiriView: View {
                 .padding([.leading, .top, .trailing], 6)
                 .padding(.bottom, -6)
                 
-                Toggle("APPLE_INTELLIGENCE_TITLE", isOn: $appleIntelligenceEnabled)
+                Toggle("APPLE_INTELLIGENCE_TITLE".localize(table: table), isOn: $appleIntelligenceEnabled)
             }
             
-            Section("SIRI_REQUESTS") {
-                Toggle("SIRI_TITLE", isOn: $siriEnabled)
-                Picker("VOICE_ACTIVATION_PHRASE_TITLE", selection: $selectedListenOption) {
+            Section("SIRI_REQUESTS".localize(table: table)) {
+                Toggle("SIRI_TITLE".localize(table: table), isOn: $siriEnabled)
+                Picker("VOICE_ACTIVATION_PHRASE_TITLE".localize(table: table), selection: $selectedListenOption) {
                     ForEach(listenOptions, id: \.self) { option in
-                        Text(option)
+                        Text(option.localize(table: table))
                     }
                 }
                 VStack(alignment: .leading) {
-                    Picker("KEYBOARD_SHORTCUT_LABEL", selection: $selectedShortcutOption) {
+                    Picker("KEYBOARD_SHORTCUT_LABEL".localize(table: table), selection: $selectedShortcutOption) {
                         ForEach(shortcutOptions, id: \.self) { option in
                             if shortcutOptions.first == option || shortcutOptions[shortcutOptions.count - 2] == option {
-                                Text(option)
+                                Text(option.localize(table: table))
                                 Divider()
                             } else {
-                                Text(option)
+                                Text(option.localize(table: table))
                             }
                         }
                     }
-                    Text("KEYBOARD_SHORTCUT_TYPE_TO_SIRI_DESCRIPTION")
+                    Text("KEYBOARD_SHORTCUT_TYPE_TO_SIRI_DESCRIPTION", tableName: table)
                         .foregroundStyle(.secondary)
                         .font(.subheadline)
                 }
-                Picker("LANGUAGE_LABEL", selection: $selectedLanguage) {
+                Picker("LANGUAGE_LABEL".localize(table: table), selection: $selectedLanguage) {
                     ForEach(languageOptions, id: \.self) { option in
                         Text(option)
                     }
                 }
                 HStack {
-                    LabeledContent("VOICE_LABEL", value: "American (Voice 4)")
-                    Button("SIRI_VOICE_BUTTON") {}
+                    LabeledContent("VOICE_LABEL".localize(table: table), value: "American (Voice 4)")
+                    Button("SIRI_VOICE_BUTTON".localize(table: table)) {}
                 }
                 HStack {
-                    Text("DELETE_HISTORY_LABEL")
+                    Text("DELETE_HISTORY_LABEL", tableName: table)
                     Spacer()
-                    Button("DELETE_HISTORY_BUTTON") {}
+                    Button("DELETE_HISTORY_BUTTON".localize(table: table)) {}
                 }
-                Text("\("GM_ASK_SIRI_DEVICE_PROCESSING_NO_SHARING_FOOTER_TEXT_MAC")\n[SIRISUGGESTIONS_OPEN_BUTTON](#)")
+                Text("\("GM_ASK_SIRI_DEVICE_PROCESSING_NO_SHARING_FOOTER_TEXT_MAC".localize(table: table))\n[\("SIRISUGGESTIONS_OPEN_BUTTON".localize(table: table))](#)")
                     .font(.subheadline)
                     .foregroundStyle(.secondary)
             }
             
             Section {
-                Text("\("GM_PRIVACY_DESCRIPTION")\n[About Apple Intelligence & Privacy…](#)")
+                Text("\("GM_PRIVACY_DESCRIPTION".localize(table: table))\n[About Apple Intelligence & Privacy…](#)")
                     .font(.subheadline)
                     .foregroundStyle(.secondary)
             } footer: {
                 Button("About Siri, Dictation & Privacy…") {}
-                Button("SIRI_RESPONSES_BUTTON") {}
+                Button("SIRI_RESPONSES_BUTTON".localize(table: table)) {}
             }
             
             Section {
-                NavigationLink("EXTERNAL_AI_MODEL_NAME") {}
-                Text("EXTERNAL_AI_MODEL_FOOTER_MAC")
+                NavigationLink("EXTERNAL_AI_MODEL_NAME".localize(table: exTable)) {}
+                Text("EXTERNAL_AI_MODEL_FOOTER_MAC", tableName: exTable)
                     .font(.subheadline)
                     .foregroundStyle(.secondary)
             } header: {
-                Text("EXTERNAL_AI_MODEL_GROUP")
+                Text("EXTERNAL_AI_MODEL_GROUP".localize(table: exTable))
             } footer: {
                 HelpButton(topicID: "mchl3fd7fc15")
             }
