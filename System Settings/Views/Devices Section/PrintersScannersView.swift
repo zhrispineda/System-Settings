b21bf69185d4b55cc314ebd59b92590381616577
zhrispineda <zhris@zhr.is>
2025-07-01 11:47:10 -0400

    Update PrintersScannersView and delete PrinterScannerSettings string catalog

--- System Settings/Views/Devices Section/PrintersScannersView.swift
+++ System Settings/Views/Devices Section/PrintersScannersView.swift
@@ -8,21 +8,21 @@
 import SwiftUI
 
 struct PrintersScannersView: View {
+    @State private var localization = LocalizationManager(bundleURL: URL(filePath: "/System/Library/ExtensionKit/Extensions/PrinterScannerSettings.appex"))
     @State private var selectedPaperSize = "US Letter"
     @State private var selectedPrinter = "PRINTER_LAST"
     let paperOptions = ["US Letter", "US Legal", "A4", "A5", "JIS B5", "B5", "Envelope #10", "Envelope DL", "Tabloid", "A3", "Tabloid Oversize", "ROC 16K", "Envelope Choukei 3", "Super B/A3"]
     let printerOptions = ["PRINTER_LAST"]
-    let table = "PrinterScannerSettings"
     
     var body: some View {
         CustomForm(title: "Printers & Scanners") {
             Section {
-                Picker("DEFAULT_PRINTER".localize(table: table), selection: $selectedPrinter) {
+                Picker("DEFAULT_PRINTER".localized(using: localization), selection: $selectedPrinter) {
                     ForEach(printerOptions, id: \.self) { option in
-                        Text(option.localize(table: table))
+                        Text(option.localized(using: localization))
                     }
                 }
-                Picker("DEFAULT_PAPER_SIZE".localize(table: table), selection: $selectedPaperSize) {
+                Picker("DEFAULT_PAPER_SIZE".localized(using: localization), selection: $selectedPaperSize) {
                     ForEach(paperOptions, id: \.self) { option in
                         Text(option)
                     }
@@ -30,14 +30,14 @@ struct PrintersScannersView: View {
             }
             
             Section {
-                Text("NO_PRINTERS_AVAILABLE", tableName: table)
+                Text("NO_PRINTERS_AVAILABLE".localized(using: localization))
                     .frame(maxWidth: .infinity)
                     .foregroundStyle(.secondary)
                     .font(.footnote)
             } footer: {
                 HStack {
                     Spacer()
-                    Button("ADD_PRINTER_ETC".localize(table: table)) {}
+                    Button("ADD_PRINTER_ETC".localized(using: localization)) {}
                     HelpButton(topicID: "prtct004")
                 }
             }
