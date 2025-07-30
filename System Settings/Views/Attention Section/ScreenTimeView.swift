04a8df155a2794392cbe4449d4569568efcd886e
Chris <zhris@zhr.is>
2025-07-30 13:27:59 -0400

    Update ScreenTimeView for localization

--- System Settings/Views/Attention Section/ScreenTimeView.swift
+++ System Settings/Views/Attention Section/ScreenTimeView.swift
@@ -8,22 +8,24 @@
 import SwiftUI
 
 struct ScreenTimeView: View {
+    @State private var table = LocalizationManager(bundleURL: "/System/Library/ExtensionKit/Extensions/ScreenTimePreferencesExtension.appex")
+    
     var body: some View {
-        CustomForm(title: "Screen Time") {
+        CustomForm(title: "Screen Time".localized(using: table)) {
             Section {
                 Placard(icon: "com.apple.graphic-icon.screen-time") {
-                    Text("Screen Time")
+                    Text("Screen Time".localized(using: table))
                     Text("""
                         Adults can set parental controls for a child's device, including app limits, content and web access, and approval requests.
-                         
+
                         You can also get insights about your personal screen time and set limits as needed.
-                        """)
+                        """.localized(using: table))
                 }
             }
             
-            Section("Limit Usage") {
+            Section("Limit Usage".localized(using: table)) {
                 NavigationLink {} label: {
-                    SettingsCell("App & Website Activity", symbol: "com.apple.prefpane.screentime.appUsage")
+                    SettingsCell("App & Website Activity".localized(using: table), symbol: "com.apple.prefpane.screentime.appUsage")
                 }
                 NavigationLink {} label: {
                     ZStack {
@@ -39,38 +41,38 @@ struct ScreenTimeView: View {
                                 .frame(width: 20)
                         }
                     }
-                    Text("Screen Distance")
+                    Text("Screen Distance".localized(using: table))
                         .padding(.leading, 2)
                 }
             }
             
-            Section("Communications") {
+            Section("Communication".localized(using: table)) {
                 NavigationLink {} label: {
-                    SettingsCell("Communication Limits", symbol: "com.apple.prefpane.screentime.communicationLimits")
+                    SettingsCell("Communication Limits".localized(using: table), symbol: "com.apple.prefpane.screentime.communicationLimits")
                 }
                 NavigationLink {} label: {
-                    SettingsCell("Communication Safety", symbol: "com.apple.prefpane.screentime.communicationSafety")
+                    SettingsCell("Communication Safety".localized(using: table), symbol: "com.apple.prefpane.screentime.communicationSafety")
                 }
             }
             
-            Section("Restrictions") {
+            Section("Restrictions".localized(using: table)) {
                 NavigationLink {} label: {
-                    SettingsCell("Content & Privacy", symbol: "com.apple.prefpane.screentime.contentPrivacy")
+                    SettingsCell("Content & Privacy".localized(using: table), symbol: "com.apple.prefpane.screentime.contentPrivacy")
                 }
             }
             
             Section {
                 Toggle(isOn: .constant(false)) {
-                    Text("Share across devices")
-                    Text("You can enable this on any device signed in to iCloud to sync your Screen Time settings.")
+                    Text("Share across devices".localized(using: table))
+                    Text("You can enable this on any device signed in to iCloud to sync your Screen Time settings.".localized(using: table))
                 }
                 .disabled(true)
             }
             
             Section {
                 Toggle(isOn: .constant(false)) {
-                    Text("Lock Screen Time Settings")
-                    Text("Use a passcode to secure Screen Time settings.")
+                    Text("Lock Screen Time Settings".localized(using: table))
+                    Text("Use a passcode to secure Screen Time settings.".localized(using: table))
                 }
                 .disabled(true)
             }
