39eb2e48cf3789158aa3ac1300f5a9586cef38b7
zhrispineda <zhris@zhr.is>
2025-08-28 13:08:38 -0500

    Update AboutView to add header

--- System Settings/Views/Main Section/General/AboutView.swift
+++ System Settings/Views/Main Section/General/AboutView.swift
@@ -8,9 +8,33 @@
 import SwiftUI
 
 struct AboutView: View {
+    let macInfo = MacInfo()
+    
     var body: some View {
         CustomForm(title: "About", root: false) {
-            
+            Section {} header: {
+                HStack {
+                    Spacer()
+                    VStack(spacing: 0) {
+                        if let deviceImage = macInfo.color() {
+                            Image(nsImage: deviceImage)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 140)
+                        }
+                        Text(macInfo.model().name)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                        Text(macInfo.model().year)
+                            .font(.subheadline)
+                            .fontWeight(.regular)
+                            .foregroundStyle(.tertiary)
+                    }
+                    Spacer()
+                }
+                
+                
+            }
         }
     }
 }
