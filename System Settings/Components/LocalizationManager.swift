34b18576de5fd71ee77b9796b4818570c6607567
zhrispineda <zhris@zhr.is>
2025-09-22 13:50:37 -0500

    Update LocalizationManager for documentation

--- System Settings/Components/LocalizationManager.swift
+++ System Settings/Components/LocalizationManager.swift
@@ -5,7 +5,18 @@
 
 import SwiftUI
 
-@Observable class LocalizationManager {
+/// A manager class responsible for configuring and providing localization.
+///
+/// - Parameters:
+///   - bundleURL: The file-system path (as String) to a bundle or framework.
+///   - stringsFile: The name of the `.strings` file to use (default: "Localizable").
+///
+/// LocalizationManager is used to initialize localization based on the user's language. The language is obtained from the UserDefaults key `AppleLanguages`.
+///
+/// It will first configure itself to the given bundleURL path and set stringsFile name. It then calls   `getLocalizable(bundleURL:stringsFile:)`, calling upon `_LSStringLocalizer` to obtain string keys located in either the language's `.lproj` folder or `.loctable` dictionary.
+///
+/// - Note: If the specified `bundleURL` or `stringsFile` parameters are not valid, the text will default to its original unmodified string.
+class LocalizationManager {
     var bundleURL: String {
         didSet { configure(stringsFile: stringsFile) }
     }
