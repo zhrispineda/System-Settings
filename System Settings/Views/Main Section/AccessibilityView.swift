c5c358c3497eb960c0b6ec32be7f03d1353018c6
zhrispineda <zhris@zhr.is>
2025-10-06 11:34:37 -0400

    Update AccessibilityView for localization

--- System Settings/Views/Main Section/AccessibilityView.swift
+++ System Settings/Views/Main Section/AccessibilityView.swift
@@ -8,85 +8,94 @@
 import SwiftUI
 
 struct AccessibilityView: View {
+    let featureLocalization = LocalizationManager(bundleURL: "/System/Library/PrivateFrameworks/AccessibilitySettingsUI.framework", stringsFile: "AccessibilitySettingsUI")
+    let titleLocalization = LocalizationManager(
+        bundleURL: "/System/Library/ExtensionKit/Extensions/AccessibilitySettingsExtension.appex",
+        stringsFile: "InfoPlist"
+    )
+    let localization = LocalizationManager(
+        bundleURL: "/System/Library/ExtensionKit/Extensions/AccessibilitySettingsExtension.appex"
+    )
+    
     var body: some View {
-        CustomForm(title: "Accessibility") {
+        CustomForm(title: "CFBundleName".localized(using: titleLocalization)) {
             Section {
                 Placard(icon: "com.apple.graphic-icon.accessibility") {
-                    Text("Accessibility")
-                    Text("\("Personalize Mac in ways that work best for you with accessibility features for vision, hearing, motor, speech, and cognition.") [\("Learn More…")](help:anchor=mh35884)")
+                    Text("CFBundleName".localized(using: titleLocalization))
+                    Text("\("accessibilityPlacardInfo".localized(using: localization)) [\("global.link.learnMore".localized(using: localization))](help:anchor=mh35884)")
                         .padding(.top, -0.5)
                 }
             }
             
-            Section("Vision") {
+            Section("category.seeing.title".localized(using: localization)) {
                 NavigationLink(value: "VoiceOver") {
-                    SettingsCell("VoiceOver", symbol: "com.apple.graphic-icon.voice-over")
+                    SettingsCell("voiceOver.toggle".localized(using: localization), symbol: "com.apple.graphic-icon.voice-over")
                 }
                 NavigationLink(value: "Zoom") {
-                    SettingsCell("Zoom", symbol: "com.apple.AccessibilityUIServer.zoom")
+                    SettingsCell("GUEST_PASS_FEATURE_ZOOM".localized(using: featureLocalization), symbol: "com.apple.AccessibilityUIServer.zoom")
                 }
                 NavigationLink(value: "Hover Text") {
-                    SettingsCell("Hover Text", symbol: "com.apple.AccessibilityUIServer.hovertext")
+                    SettingsCell("zoom.enableHoverTyping".localized(using: localization), symbol: "com.apple.AccessibilityUIServer.hovertext")
                 }
                 NavigationLink(value: "Display") {
                     SettingsCell("Display", symbol: "com.apple.accessibility-settings.icon.display")
                 }
                 NavigationLink(value: "Motion") {
-                    SettingsCell("Motion", symbol: "com.apple.graphic-icon.motion")
+                    SettingsCell("GUEST_PASS_FEATURE_MOTION".localized(using: featureLocalization), symbol: "com.apple.graphic-icon.motion")
                 }
                 NavigationLink(value: "Read & Speak") {
-                    SettingsCell("Read & Speak", symbol: "com.apple.graphic-icon.spoken-content")
+                    SettingsCell("GUEST_PASS_FEATURE_SPOKEN_CONTENT".localized(using: featureLocalization), symbol: "com.apple.graphic-icon.spoken-content")
                 }
                 NavigationLink(value: "Audio Descriptions") {
                     SettingsCell("Audio Descriptions", symbol: "com.apple.AccessibilityUIServer.audio.descriptions")
                 }
             }
             
-            Section("Hearing") {
+            Section("category.hearing.title".localized(using: localization)) {
                 NavigationLink(value: "Hearing Devices") {
                     SettingsCell("Hearing Devices", symbol: "com.apple.graphic-icon.hearing")
                 }
                 NavigationLink(value: "Audio") {
-                    SettingsCell("Audio", symbol: "com.apple.graphic-icon.sound")
+                    SettingsCell("switch.triggerType.audio.displayName".localized(using: localization), symbol: "com.apple.graphic-icon.sound")
                 }
                 NavigationLink(value: "RTT") {
-                    SettingsCell("RTT", symbol: "com.apple.graphic-icon.tty")
+                    SettingsCell("rtt.enableRtt".localized(using: localization), symbol: "com.apple.graphic-icon.tty")
                 }
                 NavigationLink(value: "Captions") {
                     SettingsCell("Captions", symbol: "com.apple.graphic-icon.subtitles-and-captioning")
                 }
                 NavigationLink(value: "Live Captions") {
-                    SettingsCell("Live Captions", symbol: "com.apple.AccessibilityUIServer.live.captions")
+                    SettingsCell("systemTranscription.toggle".localized(using: localization), symbol: "com.apple.AccessibilityUIServer.live.captions")
                 }
                 NavigationLink(value: "Name Recognition") {
-                    SettingsCell("Name Recognition", symbol: "com.apple.accessibility-settings.icon.namerecognition")
+                    SettingsCell("nameRecognition.title".localized(using: localization), symbol: "com.apple.accessibility-settings.icon.namerecognition")
                 }
             }
             
-            Section("Motor") {
+            Section("category.interacting.title".localized(using: localization)) {
                 NavigationLink(value: "Voice Control") {
-                    SettingsCell("Voice Control", symbol: "com.apple.AccessibilityUIServer.voice.control")
+                    SettingsCell("voiceControl.toggle".localized(using: localization), symbol: "com.apple.AccessibilityUIServer.voice.control")
                 }
                 NavigationLink(value: "Keyboard") {
-                    SettingsCell("Keyboard", symbol: "com.apple.graphic-icon.keyboard")
+                    SettingsCell("switch.triggerType.keyboard.displayName".localized(using: localization), symbol: "com.apple.graphic-icon.keyboard")
                 }
                 NavigationLink(value: "Pointer Control") {
-                    SettingsCell("Pointer Control", symbol: "com.apple.accessibility-settings.icon.pointercontrol")
+                    SettingsCell("GUEST_PASS_FEATURE_POINTER_CONTROL".localized(using: featureLocalization), symbol: "com.apple.accessibility-settings.icon.pointercontrol")
                 }
                 NavigationLink(value: "Switch Control") {
-                    SettingsCell("Switch Control", symbol: "com.apple.accessibility-settings.icon.switchcontrol")
+                    SettingsCell("switchControl.toggle".localized(using: localization), symbol: "com.apple.accessibility-settings.icon.switchcontrol")
                 }
             }
             
-            Section("Speech") {
+            Section("category.speech.title".localized(using: localization)) {
                 NavigationLink(value: "Live Speech") {
-                    SettingsCell("Live Speech", symbol: "com.apple.accessibility-settings.icon.livespeech")
+                    SettingsCell("liveSpeech.toggle".localized(using: localization), symbol: "com.apple.accessibility-settings.icon.livespeech")
                 }
                 NavigationLink(value: "Personal Voice") {
-                    SettingsCell("Personal Voice", symbol: "com.apple.AccessibilityUIServer.personal.voice")
+                    SettingsCell("personalVoice.title".localized(using: localization), symbol: "com.apple.AccessibilityUIServer.personal.voice")
                 }
                 NavigationLink(value: "Vocal Shortcuts") {
-                    SettingsCell("Vocal Shortcuts", symbol: "com.apple.AccessibilityUIServer.vocal.shortcuts")
+                    SettingsCell("avs.title".localized(using: localization), symbol: "com.apple.AccessibilityUIServer.vocal.shortcuts")
                 }
             }
             
@@ -95,10 +104,13 @@ struct AccessibilityView: View {
                     SettingsCell("Siri", symbol: "com.apple.application-icon.siri")
                 }
                 NavigationLink(value: "Shortcut") {
-                    SettingsCell("Shortcut", symbol: "com.apple.accessibility-settings.icon.shortcut")
+                    SettingsCell(
+                        "switch.triggerType.combo.displayName".localized(using: localization),
+                        symbol: "com.apple.accessibility-settings.icon.shortcut"
+                    )
                 }
             } header: {
-                Text("General")
+                Text("global.general".localized(using: localization))
             } footer: {
                 HelpButton(topicID: "mchlp1400")
                     .frame(maxWidth: .infinity, alignment: .trailing)
